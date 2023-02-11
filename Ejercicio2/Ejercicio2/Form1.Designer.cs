23d0fc8c0e57e68c216183907d9b80a5152dfb8e
DianaDelCid <delciddiana3@gmail.com>
2023-02-10 21:52:30 -0600

    Ejercicio Finalizado
    Ejercicio2 - Promedio Finalizado

--- Ejercicio2/Ejercicio2/Form1.Designer.cs
+++ Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -29,12 +29,218 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Nota1TextBox = new System.Windows.Forms.TextBox();
+            this.Nota2TextBox = new System.Windows.Forms.TextBox();
+            this.Nota3TextBox = new System.Windows.Forms.TextBox();
+            this.Nota4TextBox = new System.Windows.Forms.TextBox();
+            this.CalcularButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PromedioLabel = new System.Windows.Forms.Label();
+            this.BorrarButton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(163, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIENVENIDO ESTUDIANTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese Notas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "I Parcial:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "II Parcial:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(59, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "III Parcial:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(60, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "IV Parcial:";
+            // 
+            // Nota1TextBox
+            // 
+            this.Nota1TextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(216)))), ((int)(((byte)(156)))));
+            this.Nota1TextBox.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota1TextBox.Location = new System.Drawing.Point(144, 107);
+            this.Nota1TextBox.Name = "Nota1TextBox";
+            this.Nota1TextBox.Size = new System.Drawing.Size(100, 29);
+            this.Nota1TextBox.TabIndex = 6;
+            // 
+            // Nota2TextBox
+            // 
+            this.Nota2TextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(216)))), ((int)(((byte)(156)))));
+            this.Nota2TextBox.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota2TextBox.Location = new System.Drawing.Point(144, 145);
+            this.Nota2TextBox.Name = "Nota2TextBox";
+            this.Nota2TextBox.Size = new System.Drawing.Size(100, 29);
+            this.Nota2TextBox.TabIndex = 7;
+            // 
+            // Nota3TextBox
+            // 
+            this.Nota3TextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(216)))), ((int)(((byte)(156)))));
+            this.Nota3TextBox.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota3TextBox.Location = new System.Drawing.Point(144, 185);
+            this.Nota3TextBox.Name = "Nota3TextBox";
+            this.Nota3TextBox.Size = new System.Drawing.Size(100, 29);
+            this.Nota3TextBox.TabIndex = 8;
+            // 
+            // Nota4TextBox
+            // 
+            this.Nota4TextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(216)))), ((int)(((byte)(156)))));
+            this.Nota4TextBox.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota4TextBox.Location = new System.Drawing.Point(144, 224);
+            this.Nota4TextBox.Name = "Nota4TextBox";
+            this.Nota4TextBox.Size = new System.Drawing.Size(100, 29);
+            this.Nota4TextBox.TabIndex = 9;
+            // 
+            // CalcularButton
+            // 
+            this.CalcularButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.CalcularButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CalcularButton.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalcularButton.Location = new System.Drawing.Point(302, 108);
+            this.CalcularButton.Name = "CalcularButton";
+            this.CalcularButton.Size = new System.Drawing.Size(108, 31);
+            this.CalcularButton.TabIndex = 10;
+            this.CalcularButton.Text = "CALCULAR";
+            this.CalcularButton.UseVisualStyleBackColor = false;
+            this.CalcularButton.Click += new System.EventHandler(this.CalcularButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(299, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 19);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "PROMEDIO FINAL:";
+            // 
+            // PromedioLabel
+            // 
+            this.PromedioLabel.AutoSize = true;
+            this.PromedioLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.PromedioLabel.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PromedioLabel.Location = new System.Drawing.Point(459, 171);
+            this.PromedioLabel.Name = "PromedioLabel";
+            this.PromedioLabel.Size = new System.Drawing.Size(0, 21);
+            this.PromedioLabel.TabIndex = 12;
+            // 
+            // BorrarButton
+            // 
+            this.BorrarButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BorrarButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BorrarButton.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BorrarButton.Location = new System.Drawing.Point(416, 108);
+            this.BorrarButton.Name = "BorrarButton";
+            this.BorrarButton.Size = new System.Drawing.Size(108, 31);
+            this.BorrarButton.TabIndex = 13;
+            this.BorrarButton.Text = "BORRAR";
+            this.BorrarButton.UseVisualStyleBackColor = false;
+            this.BorrarButton.Click += new System.EventHandler(this.BorrarButton_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(247)))), ((int)(((byte)(223)))));
+            this.ClientSize = new System.Drawing.Size(569, 300);
+            this.Controls.Add(this.BorrarButton);
+            this.Controls.Add(this.PromedioLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.CalcularButton);
+            this.Controls.Add(this.Nota4TextBox);
+            this.Controls.Add(this.Nota3TextBox);
+            this.Controls.Add(this.Nota2TextBox);
+            this.Controls.Add(this.Nota1TextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Promedio";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Nota1TextBox;
+        private System.Windows.Forms.TextBox Nota2TextBox;
+        private System.Windows.Forms.TextBox Nota3TextBox;
+        private System.Windows.Forms.TextBox Nota4TextBox;
+        private System.Windows.Forms.Button CalcularButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label PromedioLabel;
+        private System.Windows.Forms.Button BorrarButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- Ejercicio2/Ejercicio2/Form1.cs
+++ Ejercicio2/Ejercicio2/Form1.cs
@@ -16,5 +16,71 @@ namespace Ejercicio2
         {
             InitializeComponent();
         }
+
+        private async void CalcularButton_Click(object sender, EventArgs e)
+        {
+            //Condiciones simples para que evalua si caja de texto esta vacia
+            if (Nota1TextBox.Text == "")
+            {
+                errorProvider1.SetError(Nota1TextBox, "Ingrese Nota"); //Si es verdadera muestra un error y pide que ingrese la nota
+                Nota1TextBox.Focus(); //metodo que situa el cursor en el campo nombrado
+                return;
+            }
+            if (Nota2TextBox.Text == "")
+            {
+                errorProvider1.SetError(Nota2TextBox, "Ingrese Nota"); 
+                Nota2TextBox.Focus(); 
+                return;
+            }
+            if (Nota3TextBox.Text == "")
+            {
+                errorProvider1.SetError(Nota3TextBox, "Ingrese Nota"); 
+                Nota3TextBox.Focus(); 
+                return;
+            }
+            if (Nota4TextBox.Text == "")
+            {
+                errorProvider1.SetError(Nota4TextBox, "Ingrese Nota"); 
+                Nota4TextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear(); //Limpia el control Error en la pantalla
+
+            //VARIABLES Y CONVERSIÓN DE LAS CAJAS DE TEXTO
+            decimal nota1 = Convert.ToDecimal(Nota1TextBox.Text);
+            decimal nota2 = Convert.ToDecimal(Nota2TextBox.Text);
+            decimal nota3 = Convert.ToDecimal(Nota3TextBox.Text);
+            decimal nota4 = Convert.ToDecimal(Nota4TextBox.Text);
+
+
+            decimal NotaFinal = await PromedioAsync(nota1, nota2, nota3, nota4);
+            PromedioLabel.Text = Convert.ToString(NotaFinal);
+
+        }
+
+        //FUNCIÓN ASÍNCRONA
+        //Permite devolver UNA TAREA que devuelve un valor decumal es decir el resultado final del promedio
+        private async Task<decimal> PromedioAsync(decimal n1, decimal n2, decimal n3, decimal n4)
+        {
+            //DECLARAMOS EL PROMEDIO QUE VA A RETORNAR
+            decimal promedio = await Task.Run(() =>
+            {
+                //RETORNA LA OPERACION Y LA ALMACENA EN VARIABLE PROMEDIO
+                return (n1 + n2 + n3 + n4) / 4;
+            });
+            //RETORNA LA TAREA ASINCRONA PROMEDIO 
+            return promedio;
+        }
+
+        private void BorrarButton_Click(object sender, EventArgs e)
+        {
+            //Boton que limpia los datos de las cajas de textos
+            Nota1TextBox.Text = "";
+            Nota2TextBox.Text = "";
+            Nota3TextBox.Text = "";
+            Nota4TextBox.Text = "";
+            PromedioLabel.Text = "";
+            Nota1TextBox.Focus(); //metodo que situa el cursor en el campo nombrado
+        }
     }
 }
